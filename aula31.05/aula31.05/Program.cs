5688bd12efbf17cfdae655f91d2c452166937011
Caroline Costa <carolineetiene@hotmail.com>
2017-05-31 16:09:17 -0300

    criação  de um objeto que se move pela tela 31/05

---
+++ aula31.05/aula31.05/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjetosNoPlano
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 600, L = 800;
+
+            Console.WriteLine("Digite o X inicial:  ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o y inicial:  ");
+            int y = int.Parse(Console.ReadLine());
+
+          
+            Objeto2D obj = new Objeto2D;
+ 
+           
+                obj.x = int.Parse(Console.ReadLine());
+                obj.x = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Digite a direção: ");
+            ConsoleKey direção = Console.ReadKey().Key;
+
+            if (direção  == ConsoleKey.RightArrow)
+            {
+                obj.AndarParaD;
+                Console.WriteLine(obj.Cordenadas());
+            }
+        }
+    }
+}
