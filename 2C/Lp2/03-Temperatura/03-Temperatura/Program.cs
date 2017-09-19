b355719eed2c7450e8c9e77c8e2a3bed77060d73
davimattos14 <davimattos2012@gmail.com>
2017-09-19 13:38:05 -0300

    Conversor de Temperatura (Kelvin e Celsius)

---
+++ 2C/Lp2/03-Temperatura/03-Temperatura/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Temperatura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+    
+          {
+            string resp;
+
+            TemperaturaCelsius Temp = new TemperaturaCelsius();
+
+            Console.WriteLine("INforme a temperatura em Celsius");
+            Temp.celsius = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Deseja converter para Kelvin? ");
+            resp = Console.ReadLine();
+
+
+            if (resp == "sim")
+            {
+                Console.WriteLine(Temp.ParaKelvin());
+            }
+
+            else
+                Console.WriteLine(Temp.ParaFahrenheit());
+
+        
+         }
+        
+      }
+   }
+}
